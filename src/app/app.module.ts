0d410ae5b4136647f1de01e40adcf8ef4a171dd4
Jon2may <jonathan.demay004@gmail.com>
2022-02-08 15:46:27 +0100

    create pokemon component + import icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PokedexPageComponent } from './pages/pokedex-page/pokedex-page.component';
 import { MyPokemonsPageComponent } from './pages/my-pokemons-page/my-pokemons-page.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PokedexPageComponent,
-    MyPokemonsPageComponent
+    MyPokemonsPageComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
